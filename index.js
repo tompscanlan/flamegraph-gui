417766be77c6d054652c7bf755bb2cb7f4db2e7f
Tom Scanlan <tscanaln@vmware.com>
2015-11-25 08:40:18 -0500

    allow passing perf command in

--- index.js
+++ index.js
@@ -4,38 +4,64 @@
 
 var util = require('util');
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 var sys = require('sys')
 var exec = require('child_process').exec;
 
 
 app.all('*', function(req, res, next) {
-	console.log('req: %s', util.inspect(req, {
-		showHidden: false,
-		depth: 3,
-		colors: true
-	}));
-	next();
+    console.log('req: %s', util.inspect(req, {
+        showHidden: false,
+        depth: 3,
+        colors: true
+    }));
+    next();
 })
 
+
+app.get('/image', function(req, res) {
+    var options = {
+        root: __dirname,
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+    res.sendFile('flamegraph-gui.svg', options)
+});
+
+app.post('/', function(req, res) {
+    var child = exec( req.body.command + "; perf script > out.perf; stackcollapse-perf.pl out.perf > out.folded; flamegraph.pl out.folded > flamegraph-gui.svg; ", function(error, stdout, stderr) {
+        writeForm(res);
+        res.write('<pre>stdout: ' + stdout + "</pre>");
+        res.write('<pre>stderr: ' + stderr + "</pre>");
+        if (error !== null) {
+            res.write('<pre>exec error: ' + error + "</pre>");
+        }
+        res.write('<br><a href="/image">generated image</a>');
+
+        res.end()
+    });
+});
+
 app.get('/', function(req, res) {
-
-	var child = exec("ps -ef", function(error, stdout, stderr) {
-		res.write('stdout: ' + stdout);
-		res.write('stderr: ' + stderr);
-		if (error !== null) {
-			res.write('exec error: ' + error);
-		}
-
-		res.end()
-	});
-
+    writeForm(res);
+    res.end()
 });
 
 var server = app.listen(3000, function() {
-	var host = server.address().address;
-	var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-	console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 });
+
+ function writeForm(res) {
+    res.type('html');
+    res.write('<form action=/ method="post"> <input type="text" name="command"/> <input type="submit"/> </form>');
+}
